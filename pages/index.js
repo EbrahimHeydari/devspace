e7e855e03771adb1647071f04b1f7d8704e354b8
Ebrahim <akbrly42@gmail.com>
2022-09-03 09:59:36 +0430

    11-2 Get Posts & Parse Frontmatter

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 import Layout from '../components/Layout'
 
-export default function HomePage() {
+export default function HomePage({ posts }) {
   return (
-    <Layout className='bg-blue-500'>
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
+    <Layout>
+      <h1>Hello World</h1>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map(filename => {
+    const slug = filename.replace('md', '')
+
+    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+    const { data: frontmatter } = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
