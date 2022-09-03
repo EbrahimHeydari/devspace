21ba39c7eb407796eed929f6b82163f5767780d8
Ebrahim <akbrly42@gmail.com>
2022-09-03 12:32:12 +0430

    create safelist to tailwind config for show label backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'bg-orange-400',
+    'bg-blue-400',
+    'bg-yellow-400',
+    'bg-violet-400',
+    'bg-red-400',
+  ],
   theme: {
     extend: {},
   },
