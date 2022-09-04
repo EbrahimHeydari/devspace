532c122a06aa963ffe1ae2b15d7b7989b230818f
Ebrahim <akbrly42@gmail.com>
2022-09-04 11:59:23 +0430

    12-3 Pagination Links Component

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -5,16 +5,20 @@ import Layout from '../../../components/Layout'
 import Post from '../../../components/Post'
 import { sortByDate } from '../../../utils'
 import { POST_PER_PAGE } from '../../../config'
+import Pagination from '../../../components/Pagination'
 
 export default function BlogPage({ posts, numPages, currentPage }) {
   return (
     <Layout>
       <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
+
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
         {posts.map((post, index) => (
           <Post key={index} post={post} />
         ))}
       </div>
+
+      <Pagination numPages={numPages} currentPage={currentPage} />
     </Layout>
   )
 }
@@ -56,10 +60,6 @@ export async function getStaticProps({ params }) {
   const numPages = Math.ceil(files.length / POST_PER_PAGE)
   const pageIndex = page - 1
   const orderedPost = posts.sort(sortByDate).slice(pageIndex * POST_PER_PAGE, page * POST_PER_PAGE)
-
-  console.log('page:', page)
-  console.log('numPages:', numPages)
-  console.log('pageIndex:', pageIndex)
 
   return {
     props: {
